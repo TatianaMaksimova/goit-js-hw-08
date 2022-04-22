531481c7b6705d5a543a9d54645314dba3a3145c
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-04-22 22:49:40 +0300

    finishes work with SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+const imagesMarkUp = createMarkupForImages(galleryItems);
+
+container.insertAdjacentHTML('beforeend', imagesMarkUp);
+
+function createMarkupForImages(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
