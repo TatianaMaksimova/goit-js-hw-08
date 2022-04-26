8c3691c67874955e8a9ed663c3d398c534577ef5
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-04-27 02:12:54 +0300

    adds listener for vimeo.player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+function onCurrentTime(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}
+
+player
+  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+// To remove a listener, call .off() with the callback function:
+// var callback = function() {};
+// player.off('eventName', callback);
