4bece9d5f44d8766bc8f9d580668d154580a26cb
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-04-28 00:28:35 +0300

    adds listener for submit, works with localStorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+  submit: document.querySelector('.feedback-form button'),
+};
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onSaveFormDataValue, 500));
+updatePageForm();
+
+function onSaveFormDataValue(e) {
+  formData[e.target.name] = e.target.value;
+  const formDataJSON = JSON.stringify(formData);
+
+  const savedMsg = localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+function updatePageForm(e) {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  const dataObject = JSON.parse(savedData);
+
+  if (savedData) {
+    refs.message.value = dataObject.message;
+    refs.email.value = dataObject.email;
+  }
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
+}
